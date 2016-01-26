9e2a7c89bccb441873c5a0e59066b08322afb5bb
Julia Simon <julia.solstice.simon@gmail.com>
2016-01-26 00:57:35 +0000

    bug fix in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,12 @@
 Nomster::Application.configure do
+  #config.action_mailer.default_url_options = {host: 'https://nomster-juliasolsticesimon.herokuapp.com/'}
+  
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'nomster-juliasolsticesimon.herokuapp.com',
+    :domain         => 'https://nomster-juliasolsticesimon.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
